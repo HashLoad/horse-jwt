153d5959e3434acc2a1001514fba9a7630d7a276
Matheus Hunsche <matheusarendthunsche@gmail.com>
2018-10-03 04:28:35 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # horse-jwt
 Middleware for JWT in HORSE
+
+Sample Horse Server Validate JWT
+```delphi
+uses
+  Horse, Horse.DelphiJWT;
+
+var
+  App: THorse;
+
+begin
+  App := THorse.Create(9000);
+
+  App.Use(DephiJWT('jwt-secret')); 
+
+  App.Post('ping',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    begin
+      Req.Session<TJSONObject>; //Default Payload is JSON
+    end);
+
+  App.Start;
+```
+
+Validate JWT and get custom session
+```delphi
+uses
+  Horse, Horse.DelphiJWT;
+
+var
+  App: THorse;
+
+begin
+  App := THorse.Create(9000);
+
+  App.Use(DephiJWT('jwt-secret', TMySession)); 
+
+  App.Post('ping',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    begin
+      Req.Session<TMySession>.MyField;
+    end);
+
+  App.Start;
+```
---
+++ src/Horse.JWT.pas
@@ -0,0 +1,76 @@
+unit Horse.JWT;
+
+interface
+
+uses
+  Horse, System.Classes, System.JSON, Web.HTTPApp, System.SysUtils,
+  JOSE.Core.JWT, JOSE.Core.JWK, JOSE.Core.Builder, JOSE.Consumer.Validators,
+  JOSE.Consumer, JOSE.Context, REST.JSON;
+
+procedure Middleware(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+function DelphiJWT(ASecretJWT: string): THorseCallback; overload;
+function DelphiJWT(ASecretJWT: string; ASessionClass: TClass)
+  : THorseCallback; overload;
+
+implementation
+
+var
+  SecretJWT: string;
+  SessionClass: TClass;
+
+function DelphiJWT(ASecretJWT: string): THorseCallback; overload;
+begin
+  SecretJWT := ASecretJWT;
+  Result := Middleware;
+end;
+
+function DelphiJWT(ASecretJWT: string; ASessionClass: TClass)
+  : THorseCallback; overload;
+begin
+  SecretJWT := ASecretJWT;
+  SessionClass := ASessionClass;
+  Result := Middleware;
+end;
+
+procedure Middleware(Req: THorseRequest; Res: THorseResponse;
+  Next: TProc);
+var
+  LValidations: TJOSEConsumer;
+  LJWT: TJOSEContext;
+  LToken: string;
+  LSession: TObject;
+  LJSON: TJSONObject;
+begin
+  if not Req.Headers.TryGetValue('authorization', LToken) then
+  begin
+    Res.Send('Token not found').Status(401);
+    raise EHorseCallbackInterrupted.Create;
+  end;
+
+  LToken := LToken.Replace('bearer ', '', [rfIgnoreCase]);
+
+  LValidations := TJOSEConsumerBuilder.NewConsumer.SetVerificationKey
+    (SecretJWT).SetSkipVerificationKeyValidation.
+    SetRequireExpirationTime.Build;
+
+  LJWT := TJOSEContext.Create(LToken, TJWTClaims);
+  try
+    LValidations.ProcessContext(LJWT);
+    LJSON := LJWT.GetClaims.JSON;
+
+    if Assigned(SessionClass) then
+    begin
+      LSession := SessionClass.Create;
+      TJson.JsonToObject(LSession, LJSON);
+    end
+    else
+      LSession := LJSON;
+
+    THorseHackRequest(Req).SetSession(LSession);
+  except
+    Res.Send('Unauthorized').Status(401);
+    raise EHorseCallbackInterrupted.Create;
+  end;
+end;
+
+end.
