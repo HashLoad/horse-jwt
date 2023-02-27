aa5d3f9c3d600234b2fae471d6a26584e9e4d69a
Vinicius Sanchez <viniciuss.sanchez@gmail.com>
2023-02-27 08:42:53 -0300

    session

--- samples/delphi/session/samples_session.dpr
+++ samples/delphi/session/samples_session.dpr
@@ -1,7 +1,6 @@
 program samples_session;
 
 {$APPTYPE CONSOLE}
-
 {$R *.res}
 
 uses
@@ -19,16 +18,28 @@ type
   private
     function GetuserId: string;
     procedure SetuserId(const Value: string);
-
+    function GetExemplo: string;
+    procedure SetExemplo(const Value: string);
   public
+    property exemplo: string read GetExemplo write SetExemplo;
     property userId: string read GetuserId write SetuserId;
   end;
 
 { TMyClaims }
 
+function TMyClaims.GetExemplo: string;
+begin
+  Result := TJSONUtils.GetJSONValue('exemplo', FJSON).AsString;
+end;
+
 function TMyClaims.GetuserId: string;
 begin
   Result := TJSONUtils.GetJSONValue('userId', FJSON).AsString;
+end;
+
+procedure TMyClaims.SetExemplo(const Value: string);
+begin
+  TJSONUtils.SetJSONValueFrom<string>('exemplo', Value, FJSON);
 end;
 
 procedure TMyClaims.SetuserId(const Value: string);
@@ -54,11 +65,11 @@ begin
       LJWT := TJWT.Create(TMyClaims);
       try
         LClaims := TMyClaims(LJWT.Claims);
-
         LClaims.Issuer := 'Horse';
         LClaims.Subject := 'Vinicius Sanchez';
         LClaims.Expiration := Now + 1;
         LClaims.userId := '1234';
+        LClaims.SetClaimOfType<string>('exemplo', 'teste');
         LToken := TJOSE.SHA256CompactToken('my-private-key', LJWT);
         Res.Send(TJSONObject.Create(TJSONPair.Create('token', LToken)));
       finally
@@ -77,6 +88,9 @@ begin
         LSession := Req.Session<TMyClaims>;
         LJSON := TJSONObject.Create;
         LJSON.AddPair('userId', LSession.userId);
+        LJSON.AddPair('issuer', LSession.Issuer);
+        LJSON.AddPair('subject', LSession.Subject);
+        LJSON.AddPair('exemplo', LSession.exemplo);
         Res.Send<TJSONObject>(LJSON);
       end);
 
